0bd2716bcdbe19fb2b56803370480f56acf4e634
qwqVictor <i@qwq.ren>
2022-03-09 14:54:50 +0000

    feat(5): init dict keys wth number in [1,20]

--- 5Dictionary2.py
+++ 5Dictionary2.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 # author: Victor Huang <2020215138@stu.cqupt.edu.cn>
 
+import random
+
 keys = ["dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
 dictionary = {}
+
+for key in keys:
+    dictionary[key] = random.randint(1, 20)
