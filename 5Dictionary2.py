72aef7225c1d7219b6c98f55f28b847440646a70
qwqVictor <i@qwq.ren>
2022-03-09 14:52:54 +0000

    feat(5): init keys and empty dict

---
+++ 5Dictionary2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Victor Huang <2020215138@stu.cqupt.edu.cn>
+
+keys = ["dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+
+dictionary = {}
