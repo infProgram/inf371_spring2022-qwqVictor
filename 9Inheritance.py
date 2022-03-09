bd9df7723cd9e7f2e2256fd6de16337500b96dbd
qwqVictor <i@qwq.ren>
2022-03-09 15:52:14 +0000

    feat(9): implement methods we need

--- 9Inheritance.py
+++ 9Inheritance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # author: Victor Huang <2020215138@stu.cqupt.edu.cn>
 
+import random
 # workaround for filename starts with number
 Character = __import__("8Revision").Character
 
@@ -10,4 +11,18 @@ class MagicCharacter(Character):
     def __init__(self):
         super()
         self.mana = self.intelligence * 30 + 50
+
+    def show_hitpoint_with_mana(self):
+        self.show_hitpoint()
+        print("My mana is %s!" % str(self.mana))
     
+    def magic_missle(self):
+        self.mana -= 5
+        return random.randint(5, 10)
+    
+    def fireball(self):
+        self.mana -= 10
+        return random.randint(10, 20)
+    
+    def heal_mana(self, healed):
+        self.mana += healed
