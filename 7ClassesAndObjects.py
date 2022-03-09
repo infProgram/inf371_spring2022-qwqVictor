9fc3b7af9792c069afee6f344688a5f76f1d640a
qwqVictor <i@qwq.ren>
2022-03-09 15:10:59 +0000

    feat(7): create Character class with some fields

---
+++ 7ClassesAndObjects.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Victor Huang <2020215138@stu.cqupt.edu.cn>
+
+class Character:
+    strength     = 8
+    dexterity    = 10
+    constitution = 12
+    intelligence = 9
+    wisdom       = 18
+    charisma     = 11
+
+    hitpoints    = constitution * 30 + 50
+
+    
