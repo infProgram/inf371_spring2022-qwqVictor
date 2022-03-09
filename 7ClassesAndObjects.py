201683bdf4f43dcb5f76412117d87ae47a3f7528
qwqVictor <i@qwq.ren>
2022-03-09 15:24:14 +0000

    feat(7): implement all methods we need

--- 7ClassesAndObjects.py
+++ 7ClassesAndObjects.py
@@ -2,7 +2,11 @@
 # -*- coding: utf-8 -*-
 # author: Victor Huang <2020215138@stu.cqupt.edu.cn>
 
+import random
+
 class Character:
+    __stats__  = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+
     strength     = 8
     dexterity    = 10
     constitution = 12
@@ -10,6 +14,26 @@ class Character:
     wisdom       = 18
     charisma     = 11
 
-    hitpoints    = constitution * 30 + 50
+    hitpoints = constitution * 30 + 50
 
+    def __init__(self):
+        pass
+
+    def show_stats(self):
+        for stat in self.__stats__:
+            print("%s: %s" % (stat, str(self.__getattribute__(stat))))
+
+    def show_hitpoint(self):
+        print("My hitpoint is %s!" % (str(self.hitpoints)))
     
+    def attack(self):
+        return random.randint(1, self.strength)
+    
+    def defense(self, attacked):
+        if random.randint(1, 20) < self.dexterity:
+            pass
+        else:
+            self.hitpoints -= attacked
+
+    def heal(self, healed):
+        self.hitpoints += healed
