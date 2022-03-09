552f20a90a1cfe2b5eccc90878cae850ef30a74c
qwqVictor <i@qwq.ren>
2022-03-09 14:42:46 +0000

    feat(3): make while loop running until user type q

---
+++ 3WhileLoop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Victor Huang <2020215138@stu.cqupt.edu.cn>
+
+isRunning: bool = True
+
+while isRunning:
+    if input("Do you want to quit? (q)") == "q":
+        isRunning = False
