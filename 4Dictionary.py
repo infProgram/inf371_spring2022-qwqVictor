9fbb8810d97d2d8e8b15471cb02e833cc989c907
qwqVictor <i@qwq.ren>
2022-03-09 14:46:02 +0000

    feat(4): init dictionary and print

---
+++ 4Dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: Victor Huang <2020215138@stu.cqupt.edu.cn>
+
+characterA = {
+    "strength": 8,
+    "dexterity": 10,
+    "constitution": 12,
+    "intelligence": 9,
+    "wisdom": 18,
+    "charisma": 11
+}
+
+print(characterA)
