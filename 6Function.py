de805e6609aba4d30feadb29f318d6b071c7b79a
qwqVictor <i@qwq.ren>
2022-03-09 15:07:12 +0000

    feat(6): interactive command user interface

--- 6Function.py
+++ 6Function.py
@@ -4,3 +4,10 @@
 
 def celcius2fahrenheit(celcius):
     return 1.8 * celcius + 32.0
+
+if __name__ == "__main__":
+    user_input = ""
+    while user_input.lower() != "quit":
+        user_input = input("Celcius? (or quit) ")
+        celcius = float(user_input)
+        print("Fahrenheit: %.2f" % (celcius2fahrenheit(celcius)))
