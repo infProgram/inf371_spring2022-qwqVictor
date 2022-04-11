9622abe67871af3632a6c7e4ddfc32afe251e672
qwqVictor <i@qwq.ren>
2022-04-11 22:41:17 +0800

    fix(pygame_practices.4): fix border bug

--- pygame_practices/4MoveItemWithKey.py
+++ pygame_practices/4MoveItemWithKey.py
@@ -14,21 +14,22 @@ class Ball:
 
     def __init__(self, color: tuple[int], radius: int, window_size: tuple[int], speed: tuple[int]):
         self.speed = [x for x in speed]
+        self.radius = radius
         self.window_size = window_size
         self.ball = pygame.Surface((radius * 2, radius * 2))
         self.ball_rect = pygame.draw.circle(self.ball, color, (radius, radius), radius).move((random.randint(radius, window_size[0] - radius), random.randint(radius, window_size[1] - radius)))
 
     def move(self, direction: tuple[int]):
-        print(direction)
+        print((self.ball_rect.x, self.ball_rect.y))
         self.ball_rect = self.ball_rect.move(*map(lambda x, y: x * y, self.speed, direction))
-        if self.ball_rect.left <= 0:
-            self.ball_rect = self.ball_rect.move((0, self.ball_rect.y))
-        elif self.ball_rect.right >= self.window_size[0]:
-            self.ball_rect = self.ball_rect.move((self.window_size[0], self.ball_rect.y))
-        if self.ball_rect.top <= 0:
-            self.ball_rect = self.ball_rect.move((self.ball_rect.x, 0))
-        elif self.ball_rect.bottom >= self.window_size[1]:
-            self.ball_rect = self.ball_rect.move((self.ball_rect.x, self.window_size[1]))
+        if self.ball_rect.x <= 0:
+            self.ball_rect.x = 0
+        elif self.ball_rect.x >= self.window_size[0]:
+            self.ball_rect.x = self.window_size[0]
+        if self.ball_rect.y <= 0:
+            self.ball_rect.y = 0
+        elif self.ball_rect.y >= self.window_size[1]:
+            self.ball_rect.y = self.window_size[1]
     
     def blit(self, screen: pygame.Surface):
         screen.blit(self.ball, self.ball_rect)
