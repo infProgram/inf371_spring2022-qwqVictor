78d5106cb269d1f73fff4ac2c83cfebec080e2a6
qwqVictor <i@qwq.ren>
2022-04-11 14:32:48 +0800

    feat(pygame_practices.1): make the ball running

--- pygame_practices/1SimpleTutorial.py
+++ pygame_practices/1SimpleTutorial.py
@@ -1,15 +1,14 @@
 import sys
 import pygame
+import random
 
 pygame.init()
 window_size = (640, 480)
 screen = pygame.display.set_mode(window_size)
-
+speed = [10, 10]
 ball = pygame.Surface((50, 50))
-ball_rect = pygame.draw.circle(ball, (0xfb, 0x72, 0x99), (25, 25), 25)
+ball_rect = pygame.draw.circle(ball, (0xfb, 0x72, 0x99), (25, 25), 25).move((random.randint(25, window_size[0] - 25), random.randint(25, window_size[1] - 25)))
 
-screen.blit(ball, ball_rect)
-pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
@@ -18,6 +17,13 @@ while running:
             running = False
             break
     if running:
-        pass
+        ball_rect = ball_rect.move(*speed)
+        if ball_rect.left <= 0 or ball_rect.right >= window_size[0]:
+            speed[0] = -speed[0]
+        if ball_rect.top <= 0 or ball_rect.bottom >= window_size[1]:
+            speed[1] = -speed[1]
+        screen.fill((0, 0, 0))
+        screen.blit(ball, ball_rect)
+        pygame.display.flip()
 
 pygame.quit()
