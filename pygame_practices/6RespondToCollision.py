e2f6530234bd45f7ddbd93f6ea15187822730841
qwqVictor <i@qwq.ren>
2022-04-12 19:32:36 +0800

    feat(pygame_practices.6): demo collision

---
+++ pygame_practices/6RespondToCollision.py
@@ -0,0 +1,32 @@
+import sys
+from time import sleep
+import pygame
+from item import Item
+
+pygame.init()
+window_size = (640, 480)
+screen = pygame.display.set_mode(window_size)
+background_color = (0, 0, 0)
+
+shark_ball = Item(picture_path="shark_ball.png", picture_size=(80, 80), window_size=window_size, speed=(4, 4))
+spider_ball = Item(picture_path="spider_ball.png", picture_size=(80, 80), window_size=window_size, speed=(5, 5))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("user exit", file=sys.stderr)
+            running = False
+            break
+    if running:
+        screen.fill((background_color))
+        shark_ball.move()
+        shark_ball.collide(spider_ball)
+        shark_ball.blit(screen)
+        spider_ball.move()
+        spider_ball.collide(shark_ball)
+        spider_ball.blit(screen)
+        pygame.display.flip()
+        sleep(1/120)
+
+pygame.quit()
