372f91495920cad15f4e16452accb6b99e1564c8
qwqVictor <i@qwq.ren>
2022-06-14 01:02:27 +0800

    feat: add respawn of ball

--- breakout/Ball.py
+++ breakout/Ball.py
@@ -4,18 +4,24 @@ from ImageSprite import ImageSprite
 class Ball(ImageSprite):
     window_width: int
     window_height: int
+    spawn_pos: tuple[int]
+    spawn_speed: tuple[int]
     speed_x: int
     speed_y: int
     head_y: int
-    def __init__(self, window: pygame.Surface, speed_x: int, speed_y: int, spawn: tuple[int], head_y: int=0):
+    def __init__(self, window: pygame.Surface, speed_x: int, speed_y: int, spawn_pos: tuple[int], head_y: int=0):
         ImageSprite.__init__(self, "ball.png")
         self.window_width = window.get_width()
         self.window_height = window.get_height()
-        self.rect.bottom = self.window_height - spawn[1]
-        self.rect.left = spawn[0]
-        self.speed_x = speed_x
-        self.speed_y = speed_y
+        self.spawn_pos = spawn_pos
+        self.spawn_speed = (speed_x, speed_y)
         self.head_y = head_y
+        self.respawn()
+
+    def respawn(self):
+        self.rect.bottom = self.window_height - self.spawn_pos[1]
+        self.rect.left = self.spawn_pos[0]
+        self.speed_x, self.speed_y = self.spawn_speed
 
     def update(self):
         self.rect = self.rect.move(self.speed_x, self.speed_y)
