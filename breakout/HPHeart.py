fb8de59c29e6d1923d55951438bc9bfcae8f4719
qwqVictor <i@qwq.ren>
2022-06-14 00:48:06 +0800

    fix: HP sprites default value issue and argpassing

--- breakout/HPHeart.py
+++ breakout/HPHeart.py
@@ -3,9 +3,9 @@ from ImageSprite import MatricedImageSprite
 # Note: same sized hp.png and hp_no.png is required.
 
 class HPHeartFulfilled(MatricedImageSprite):
-    def __init__(self, coordinate: tuple[int], margin: tuple[int]=(5, 2), base_offset: tuple[int]=(1, 10)):
-        MatricedImageSprite.__init__(self, "hp.png", margin, base_offset)
+    def __init__(self, coordinate: tuple[int], margin: tuple[int]=(1, 0), base_offset: tuple[int]=(1, 10)):
+        MatricedImageSprite.__init__(self, "hp.png", coordinate, margin, base_offset)
 
 class HPHeartLost(MatricedImageSprite):
-    def __init__(self, coordinate: tuple[int], margin: tuple[int]=(5, 2), base_offset: tuple[int]=(1, 10)):
-        MatricedImageSprite.__init__(self, "hp_no.png", margin, base_offset)
+    def __init__(self, coordinate: tuple[int], margin: tuple[int]=(1, 0), base_offset: tuple[int]=(1, 10)):
+        MatricedImageSprite.__init__(self, "hp_no.png", coordinate, margin, base_offset)
