8906ad6bfaae443097e47f2a6179abfdd4a6619f
qwqVictor <i@qwq.ren>
2022-06-13 22:24:31 +0800

    refactor: remove redudant colorkey for we are using PNG assets

--- breakout/ImageSprite.py
+++ breakout/ImageSprite.py
@@ -3,5 +3,4 @@ class ImageSprite(pygame.sprite.Sprite):
     def __init__(self, image_file: str):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('assets/' + image_file).convert()
-        self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect()
