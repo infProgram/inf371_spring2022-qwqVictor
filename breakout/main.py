7e6b37bb744b2cce2a8ac3b0cdea56732448ac2a
qwqVictor <i@qwq.ren>
2022-06-14 00:19:43 +0800

    feat: mini heading

--- breakout/main.py
+++ breakout/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from cv2 import line
 import pygame
 from Ball import Ball
 from Bat import Bat
@@ -10,13 +11,18 @@ pygame.key.set_repeat(1, 5)
 window_size = (640, 480)
 window = pygame.display.set_mode(window_size)
 background_color = (0, 0, 0)
+heading_top = 30
 clock = pygame.time.Clock()
+
+def heading(window: pygame.Surface):
+
+    pygame.draw.line(window, (0xfb, 0x72, 0x99), (0, heading_top), (window_size[0], heading_top))
 
 def main():
     all_sprites = pygame.sprite.Group()
     bouncable_sprites = pygame.sprite.Group()
     bat = Bat(window, speed=2)
-    ball = Ball(window, speed_x=2, speed_y=-2, spawn=(window.get_width() / 2, bat.rect.height))
+    ball = Ball(window, speed_x=2, speed_y=-2, spawn=(window.get_width() / 2, bat.rect.height), head_y=30)
     all_sprites.add(bat, ball)
     bouncable_sprites.add(bat)
     score = 0
@@ -52,6 +58,7 @@ def main():
                         bouncable_sprites.remove(brick)
 
             window.fill((background_color))
+            heading(window)
             all_sprites.draw(window)
             all_sprites.update()
             pygame.display.flip()
