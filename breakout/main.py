bc9c9d0897a3b4b2058dc71199154e2bacfa1cd3
qwqVictor <i@qwq.ren>
2022-06-13 20:50:57 +0800

    refactor: init pygame

--- breakout/main.py
+++ breakout/main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 import sys
-
+import pygame
 
 def main(argv: "list[str]"):
-    pass
+    size = width, height = 640, 480
+    pygame.init()
+    window = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+
+    while True:
+        
+
+        clock.tick(60)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main(sys.argv)
