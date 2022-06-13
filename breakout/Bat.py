fb6729bdb2a4b965e5d8fb2584a80521f02c6201
qwqVictor <i@qwq.ren>
2022-06-13 21:54:55 +0800

    feat: add bat class

---
+++ breakout/Bat.py
@@ -0,0 +1,22 @@
+import pygame
+from breakout.ImageSprite import ImageSprite
+
+class Bat(ImageSprite):
+    speed: int
+    window_width: int
+    window_height: int
+    def __init__(self, window: pygame.Surface, speed=4):
+        ImageSprite("bat.png")
+        self.speed = speed
+        self.window_width = window.get_width()
+        self.window_height = window.get_height()
+        self.rect.bottom = self.window_height
+        self.rect.left = (self.window_width - self.image.get_width()) / 2
+
+    def move_left(self):
+        if self.rect.left > 0:
+            self.rect.move_ip(-self.speed, 0)
+
+    def move_right(self):
+        if self.rect.right < self.window_width:
+            self.rect.move_ip(self.speed, 0)
