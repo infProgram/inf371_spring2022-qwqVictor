03ba49d3709643b0c9924bb868c99cf16380cdac
qwqVictor <i@qwq.ren>
2022-06-13 21:34:04 +0800

    feat: calculate position inside class

--- breakout/Brick.py
+++ breakout/Brick.py
@@ -2,7 +2,10 @@ import pygame
 from breakout.ImageSprite import ImageSprite
 
 class Brick(ImageSprite):
-    def __init__(self, x: int, y: int):
+    def __init__(self, coordinate: tuple[int], margin: tuple[int]=(5, 2), base_offset: tuple[int]=(40, 50)):
         ImageSprite("brick.png")
-        self.rect.x = x
-        self.rect.y = y
+        x, y = coordinate
+        margin_x, margin_y = margin
+        offset_x, offset_y = base_offset
+        self.rect.x = x * (self.rect.width + margin_x) + offset_x
+        self.rect.y = y * (self.rect.width + margin_y) + offset_y
