1a138b9e7c9447d7330ba63d7daf92e85a976b2a
qwqVictor <i@qwq.ren>
2022-06-13 22:21:57 +0800

    feat: set up basic sprite group

--- breakout/main.py
+++ breakout/main.py
@@ -7,9 +7,11 @@ def main(argv: "list[str]"):
     pygame.init()
     window = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
-
+    all_sprites = pygame.sprite.Group()
+    bouncable_sprites = pygame.sprite.Group()
+    
     while True:
-        
+
 
         clock.tick(60)
         pygame.display.flip()
